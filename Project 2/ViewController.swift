b69f9f62b258ec5dc532520c47e9eed8672d5eb6
Ednan <52186220+ednanf@users.noreply.github.com>
2023-04-14 10:14:57 -0300

    Designing your layout – Guess the Flag, part 5 -- complete

--- Project 2/ViewController.swift
+++ Project 2/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         // This will shuffle the array, so the flags will be different each time, even if we always call for index 0, 1 and 2.
         correctAnswer = Int.random(in: 0...2)
@@ -56,5 +56,24 @@ class ViewController: UIViewController {
         button3.setImage(UIImage (named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    // IBAction triggers the code.
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
 }
