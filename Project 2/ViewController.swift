69dd60864956ea65f9b6849b3c7f4b713399aef1
Ednan <52186220+ednanf@users.noreply.github.com>
2023-04-14 07:49:31 -0300

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Ednan R. Frizzera Filho on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
